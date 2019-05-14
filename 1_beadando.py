d8d662bba129973fe8f3f75bb01b6d6262b98236
hamvasimark <47623947+hamvasimark@users.noreply.github.com>
2019-05-14 15:27:32 +0200

    Add files via upload
    első feladat kész

--- 1_beadando.py
+++ 1_beadando.py
@@ -1,1 +1,19 @@
 print("első feladat")
+
+
+def hatvanyosszeg(szam):
+    lista = []
+    a = 0
+    szam2 = szam
+    while a != szam:
+        i = 0
+        while 2 ** i <= szam2:
+            i += 1
+        szam2 -= 2 ** (i - 1)
+        lista.append(i - 1)
+        a += 2 ** (i - 1)
+    return sorted(lista)
+
+
+szam = 69
+print(hatvanyosszeg(szam))
